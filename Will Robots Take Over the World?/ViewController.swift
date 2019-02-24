6c3269113b7afa81018b2a3ca283ab7d6e71bfdd
Milo Wyner <milowyner@gmail.com>
2019-02-24 13:14:25 -0800

    Apply background gradient

--- Will Robots Take Over the World?/ViewController.swift
+++ Will Robots Take Over the World?/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Layer for applying gradient
+    var gradientLayer: CAGradientLayer!
+    
+    // Light and dark colors used for background gradient
+    var lightColor = UIColor(named: "clearDayLight")!.cgColor
+    var darkColor = UIColor(named: "clearDayDark")!.cgColor
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Apply gradient to background
+        gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [lightColor, darkColor]
+        self.view.layer.addSublayer(gradientLayer)
     }
 
 

55f7407828742672e7b9c5648db9ec8b4f4f11ac
Milo Wyner <milowyner@gmail.com>
2019-03-01 17:14:47 -0800

    Import Alamofire and SwiftyJSON libraries

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Will Robots Take Over the World?' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Will Robots Take Over the World?
+  pod 'Alamofire'
+  pod 'SwiftyJSON', '~> 4.0'
+
+end
--- Will Robots Take Over the World?/ViewController.swift
+++ Will Robots Take Over the World?/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -16,12 +18,32 @@ class ViewController: UIViewController {
     var lightColor = UIColor(named: "clearDayLight")!
     var darkColor = UIColor(named: "clearDayDark")!
     
+    // Dark Sky URL
+    let url = "https://api.darksky.net/forecast/5fd28bb80945cf58fd60c4c8a3acb413/37.8267,-122.4233?exclude=minutely,hourly,daily,alerts,flags"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set colors for gradient view
         gradientView.firstColor = lightColor
         gradientView.secondColor = darkColor
+        
+        // Networking
+        
+        Alamofire.request(url).responseJSON { response in
+            if let jsonResult = response.result.value {
+                print("JSON: \(jsonResult)") // serialized json response
+                
+                let json = JSON(jsonResult)
+                print("SwiftyJSON: \(json)")
+
+                let temperature = json["currently"]["temperature"].double
+                print(temperature!)
+                
+            } else {
+                print(response.error!)
+            }
+        }
     }
 
 

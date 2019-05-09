9271590d7f5195efeb461e38f59dba8d703df32d
Milo Wyner <milowyner@gmail.com>
2019-05-09 15:23:54 -0700

    Comment more debugging code

--- Will Robots Take Over the World Today?/Controller/AppDelegate.swift
+++ Will Robots Take Over the World Today?/Controller/AppDelegate.swift
@@ -32,7 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        print("enter foreground")
         let weatherVC = window?.rootViewController as! WeatherViewController
         if weatherVC.networkManager?.networkReachabilityStatus != .notReachable {
             weatherVC.enableBasicLocationServices()
